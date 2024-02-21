57eaa25d644db1358ec6dabcbcd576cba89ba740
Laura Oliveira <112412213+laura-olivr@users.noreply.github.com>
2024-02-21 13:55:34 -0400

    Create metodos.c

---
+++ metodos.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define M 16384
+
+//key == k tam == m
+
+int metDivisao(int key){
+    return key % M;
+}
+
+
+int metMultiplicacao(int key){
+    double A = (sqrt(5) - 1)/2;
+    //printf("%f\n", A);
+    int piso = key * A;
+    //printf("%d\n", piso);
+    double frac = (key * A) - piso;
+    //printf("%f\n", frac);
+    
+    return M * frac;
+}
+
+int main(){
+    printf("%d\n", metMultiplicacao(123456));
+    return 0;
+}
