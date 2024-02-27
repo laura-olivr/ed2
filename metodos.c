c8fb774ed89ef06274015a319685113008dd1741
Laura Oliveira <112412213+laura-olivr@users.noreply.github.com>
2024-02-26 21:22:59 -0400

    Update metodos.c

--- metodos.c
+++ metodos.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
 #define M 16384
+#define A (sqrt(5) - 1)/2    //Melhor A segundo Knuth
 //#define A 0.5
 
 //key == k tam == m
@@ -11,14 +11,9 @@ int metDivisao(int key){
     return key % M;
 }
 
-
 int metMultiplicacao(int key){
-    double A = (sqrt(5) - 1)/2;
-    //printf("%f\n", A);
     int piso = key * A;
-    //printf("%d\n", piso);
     double frac = (key * A) - piso;
-    //printf("%f\n", frac);
     
     return M * frac;
 }
